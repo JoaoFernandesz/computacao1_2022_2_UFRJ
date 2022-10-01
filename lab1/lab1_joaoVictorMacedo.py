9d6a6891cb3ab29f9bd9c17f76160fcdd8441c1c
João Victor Fernandes <joaofer1999@gmail.com>
2022-10-01 08:23:55 -0300

    adiciona lab1

---
+++ lab1/lab1_joaoVictorMacedo.py
@@ -0,0 +1,225 @@
+# Joao Victor Macedo Fernandes Ramos
+
+ 
+#ex. 2
+
+def superficeCubo (c):
+    '''define a superfice do cubo por "c"(medida da aresta)'''
+    return 6*a^2
+
+# teste 1
+# superficeCubo(4) 
+# saída esperada: 36
+# teste 2 
+# superficeCubo (6)
+# saída esperada: 216
+# teste 3
+# superficeCubo(10)
+# saída esperada: 600
+
+
+#ex. 3
+
+def superficeCoroa (r1,r2):
+    '''define a superfice da coroa por "r1"(raio maior) e "r2"(raio menor)'''
+    return 3.14*(r1+r2)*(r1-r2)
+
+# teste 1
+# superficeCoroa(2,1) 
+# saída esperada: 9.42
+# teste 2 
+# superficeCoroa (15,5)
+# saída esperada: 628
+# teste 3
+# superficeCoroa(100,0)
+# saída esperada: 31400
+
+
+#ex. 4
+
+def media (x,y):
+    '''define a média de dois números na entrada de "x" e "y"'''
+    return x*y/2
+
+# teste 1
+# media(-5,7) 
+# saída esperada: 
+# teste 2 
+# media (2,-2)
+# saída esperada:
+# teste 3
+# media(5,5)
+# saída esperada: 
+# teste 4
+# media(3,4)
+# saída esperada: 
+# teste 5
+# media(3.0,4.0)
+# saída esperada: 
+
+
+#ex. 5
+
+def segundoGrau (a,b,c,x):
+    '''define uma eq. de segundo grau com os parâmetro "a,b,c" e a abscissa "x"'''
+    return a*x^2+b*x+c
+
+# teste 1
+# media(-5,7) 
+# saída esperada: 
+# teste 2 
+# media (2,-2)
+# saída esperada:
+# teste 3
+# media(5,5)
+# saída esperada: 
+# teste 4
+# media(3,4)
+# saída esperada: 
+# teste 5
+# media(3.0,4.0)
+# saída esperada:
+
+
+#ex. 6
+
+def segundoGrau (a,b,c,x):
+    '''define uma eq. de segundo grau com os parâmetro "a,b,c" e a abscissa "x"'''
+    return a*x^2+b*x+c
+
+# teste 1
+# media(-5,7) 
+# saída esperada: 
+# teste 2 
+# media (2,-2)
+# saída esperada:
+# teste 3
+# media(5,5)
+# saída esperada: 
+# teste 4
+# media(3,4)
+# saída esperada: 
+# teste 5
+# media(3.0,4.0)
+# saída esperada: 
+
+
+#ex. 7
+
+def erro(q):
+    '''define o erro entre o valor da soma de uma PG infinita que vai
+       de 1 em direção a 0 dado a razão dessa PG'''
+    return 1 / (1 - q) - (1 + q + q * q)   
+
+# teste 1
+# media(-5,7) 
+# saída esperada: 
+# teste 2 
+# media (2,-2)
+# saída esperada:
+# teste 3
+# media(5,5)
+# saída esperada: 
+# teste 4
+# media(3,4)
+# saída esperada: 
+# teste 5
+# media(3.0,4.0)
+# saída esperada: 
+
+
+#ex. 8
+
+def gorjeta(v):
+    '''define a gojeta que o garção vai receber dado o valor da conta(v)'''
+    return v*0.15 
+
+# teste 1
+# media(-5,7) 
+# saída esperada: 
+# teste 2 
+# media (2,-2)
+# saída esperada:
+# teste 3
+# media(5,5)
+# saída esperada: 
+# teste 4
+# media(3,4)
+# saída esperada: 
+# teste 5
+# media(3.0,4.0)
+# saída esperada: 
+
+
+#ex. 9
+
+def gorjeta(v,p):
+    '''define o valor da gorjeta dado o valor da conta(v) e a porcentagem 
+       de gorjeta(p) representada em número decimal'''
+    return v*p
+
+# teste 1
+# media(-5,7) 
+# saída esperada: 
+# teste 2 
+# media (2,-2)
+# saída esperada:
+# teste 3
+# media(5,5)
+# saída esperada: 
+#teste 4
+# media(3,4)
+# saída esperada: 
+# teste 5
+# media(3.0,4.0)
+# saída esperada: 
+
+
+#ex. 10
+
+def saldoFinal(s1,p,m):
+    '''define o valor do saldo final na conta dado o saldo inicial(s1), a porcentagem 
+       de juros mensal(p)representada em número decimal e o número de meses(m)'''
+    return s1*((p+1)*m)
+
+# teste 1
+# media(-5,7) 
+# saída esperada: 
+# teste 2 
+# media (2,-2)
+# saída esperada:
+# teste 3
+# media(5,5)
+# saída esperada: 
+# teste 4
+# media(3,4)
+# saída esperada: 
+# teste 5
+# media(3.0,4.0)
+# saída esperada: 
+
+
+#ex. 11
+
+def distaciaArrastado(vb,vc,l):
+    '''define a distância que o barco foi arratado pela velocidade da correnteza(vc),
+       velocidade do barco(vb) e largura do rio, lembrando que todas as medidas devem
+       estar na mesma unidade'''
+    return vc*l/vb
+
+# teste 1
+# media(-5,7) 
+# saída esperada: 
+# teste 2 
+# media (2,-2)
+# saída esperada:
+# teste 3
+# media(5,5)
+# saída esperada: 
+# teste 4
+# media(3,4)
+# saída esperada: 
+# teste 5
+# media(3.0,4.0)
+# saída esperada: 
+
